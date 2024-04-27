feb3148d319fe98a10227e0d6e0bb76f410a0fa2
TanmaySe <140996962+TanmaySe@users.noreply.github.com>
2024-04-27 18:19:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const app = express()
+const app = express();
+
 app.use(express.json());
-app.get('/api',async(req,res) => {
-    res.json({message:"testing flutter with nodeJS"})
-})
+
+app.get('/', async (req, res) => {
+    res.json({ message: "vocab-api is running now..." });
+});
+app.get('/api', async (req, res) => {
+    res.json({ message: "testing flutter with nodeJS" });
+});
+
+const PORT = 3000;
+app.listen(PORT)
